74619833c52dfb63d07125f8cf452e4d0cbf1a13
Fred4106 <natruf@gmail.com>
2012-09-12 13:36:11 -0500

    Does not work. Can not addslot.

--- src/common/Magecraft/Common/Containers/ContainerOrb.java
+++ src/common/Magecraft/Common/Containers/ContainerOrb.java
@@ -1,15 +1,16 @@
 package Magecraft.Common.Containers;
 
+import Magecraft.Common.GUI.Slots.SlotOrb;
 import Magecraft.Common.TileEntity.TileEntityOrb;
-import net.minecraft.src.Container;
-import net.minecraft.src.EntityPlayer;
-import net.minecraft.src.InventoryPlayer;
+import net.minecraft.src.*;
 
 public class ContainerOrb extends Container{
 
-	public ContainerOrb(InventoryPlayer par1InventoryPlayer, TileEntityOrb par2TileEntityIronFurnace)
+	public ContainerOrb(InventoryPlayer par1InventoryPlayer, TileEntityOrb par2TileEntityOrb)
     {
-        
+		this.addSlotToContainer(new Slot(par2TileEntityOrb, 0, 56, 17));
+        this.addSlotToContainer(new Slot(par2TileEntityOrb, 1, 56, 53));
+        this.addSlotToContainer(new SlotOrb(par1InventoryPlayer, par2TileEntityOrb, 2, 116, 35));
     }
 	
 	@Override
--- src/common/Magecraft/Common/TileEntity/TileEntityOrb.java
+++ src/common/Magecraft/Common/TileEntity/TileEntityOrb.java
@@ -1,9 +1,11 @@
 package Magecraft.Common.TileEntity;
 
+import net.minecraft.src.ItemStack;
 import net.minecraft.src.TileEntity;
 
 public class TileEntityOrb extends TileEntity
 {
+	private ItemStack[] orbItemStacks = new ItemStack[3];
 	
 	public void updateEntity()
 	{
