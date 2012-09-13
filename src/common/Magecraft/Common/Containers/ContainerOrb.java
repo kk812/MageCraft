096085d6eed4346676426bb3c53961675015b96f
Fred4106 <natruf@gmail.com>
2012-09-12 20:44:35 -0500

    TileEntity Gui now works.

--- src/common/Magecraft/Common/Containers/ContainerOrb.java
+++ src/common/Magecraft/Common/Containers/ContainerOrb.java
@@ -14,9 +14,9 @@ public class ContainerOrb extends Container{
 
 	public ContainerOrb(InventoryPlayer par1InventoryPlayer, TileEntityOrb te)
     {
-		this.addSlotToContainer(new Slot(par1InventoryPlayer, 0, 56, 17));
-        this.addSlotToContainer(new Slot(par1InventoryPlayer, 1, 56, 53));
-        this.addSlotToContainer(new SlotOrb(par1InventoryPlayer.player, par1InventoryPlayer, 2, 116, 35));
+		this.addSlotToContainer(new Slot(te, 0, 56, 17));
+        this.addSlotToContainer(new Slot(te, 1, 56, 53));
+        this.addSlotToContainer(new SlotOrb(par1InventoryPlayer.player, te, 2, 116, 35));
         
         for (int var3 = 0; var3 < 3; ++var3)
         {
--- src/common/Magecraft/Common/TileEntity/TileEntityOrb.java
+++ src/common/Magecraft/Common/TileEntity/TileEntityOrb.java
@@ -1,15 +1,124 @@
 package Magecraft.Common.TileEntity;
 
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.IInventory;
 import net.minecraft.src.ItemStack;
+import net.minecraft.src.NBTTagCompound;
+import net.minecraft.src.NBTTagList;
 import net.minecraft.src.TileEntity;
 
-public class TileEntityOrb extends TileEntity
+public class TileEntityOrb extends TileEntity implements IInventory
 {
-	private ItemStack[] orbItemStacks = new ItemStack[3];
+	public ItemStack[] orbItemStacks;
+	
+	public TileEntityOrb()
+	{
+		orbItemStacks = new ItemStack[3];
+	}
 	
 	public void updateEntity()
 	{
 		super.updateEntity();
 	}
-	
+
+	@Override
+    public int getSizeInventory() {
+            return orbItemStacks.length;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int slot) {
+            return orbItemStacks[slot];
+    }
+    
+    @Override
+    public void setInventorySlotContents(int slot, ItemStack stack) {
+    		orbItemStacks[slot] = stack;
+            if (stack != null && stack.stackSize > getInventoryStackLimit()) {
+                    stack.stackSize = getInventoryStackLimit();
+            }               
+    }
+
+    @Override
+    public ItemStack decrStackSize(int slot, int amt) {
+            ItemStack stack = getStackInSlot(slot);
+            if (stack != null) {
+                    if (stack.stackSize <= amt) {
+                            setInventorySlotContents(slot, null);
+                    } else {
+                            stack = stack.splitStack(amt);
+                            if (stack.stackSize == 0) {
+                                    setInventorySlotContents(slot, null);
+                            }
+                    }
+            }
+            return stack;
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int slot) {
+            ItemStack stack = getStackInSlot(slot);
+            if (stack != null) {
+                    setInventorySlotContents(slot, null);
+            }
+            return stack;
+    }
+    
+    @Override
+    public int getInventoryStackLimit() {
+            return 64;
+    }
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer player) {
+            return worldObj.getBlockTileEntity(xCoord, yCoord, zCoord) == this &&
+            player.getDistanceSq(xCoord + 0.5, yCoord + 0.5, zCoord + 0.5) < 64;
+    }
+
+    @Override
+    public void openChest() {}
+
+    @Override
+    public void closeChest() {}
+    
+    @Override
+    public void readFromNBT(NBTTagCompound tagCompound) 
+    {
+    	super.readFromNBT(tagCompound);
+        NBTTagList var2 = tagCompound.getTagList("Items");
+        this.orbItemStacks = new ItemStack[this.getSizeInventory()];
+
+        for (int var3 = 0; var3 < var2.tagCount(); ++var3)
+        {
+            NBTTagCompound var4 = (NBTTagCompound)var2.tagAt(var3);
+            byte var5 = var4.getByte("Slot");
+
+            if (var5 >= 0 && var5 < this.orbItemStacks.length)
+            {
+                this.orbItemStacks[var5] = ItemStack.loadItemStackFromNBT(var4);
+            }
+        }
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound tagCompound) {
+            super.writeToNBT(tagCompound);
+            NBTTagList itemList = new NBTTagList();
+            
+            for (int i = 0; i < orbItemStacks.length; i++) {
+                    ItemStack stack = orbItemStacks[i];
+                    if (stack != null) {
+                            NBTTagCompound tag = new NBTTagCompound();
+                            tag.setByte("Slot", (byte) i);
+                            stack.writeToNBT(tag);
+                            itemList.appendTag(tag);
+                    }
+            }
+            tagCompound.setTag("Orb", itemList);
+    }
+
+	@Override
+	public String getInvName() {
+		return "Orb";
+	}
 }
